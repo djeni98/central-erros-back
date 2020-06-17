2386b0129aeab6107b5536d9ef60382905ab195f
Djenifer R. P <djeniferrenata@yahoo.com.br>
2020-06-16 21:21:58 -0300

    Adiciona djangorestframework e inicia app api

--- centralErros/settings.py
+++ centralErros/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Terceiros
+    'rest_framework',
+    # Meus apps
+    'api'
 ]
 
 MIDDLEWARE = [
