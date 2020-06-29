a6524f5328660108b2512237b121ac4a48c885bc
Djenifer R. P <djeniferrenata@yahoo.com.br>
2020-06-29 08:38:01 -0300

    Muda lógica do UserView para UserSerializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,22 +1,39 @@
-from django.contrib.auth import password_validation
+from django.contrib.auth.password_validation import validate_password
 
 from rest_framework import serializers
-from rest_framework.fields import Field
 
 from api.models import User, Event, Agent
 
 
 class UserModelSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        validators=[validate_password],
+        style={'input_type': 'password'}
+    )
+
     class Meta:
         model = User
         fields = '__all__'
-        read_only_fields = ['created_at']
+        read_only_fields = ['created_at', 'last_login']
 
     def to_representation(self, instance):
         rep = super().to_representation(instance)
         if 'password' in rep:
             del rep['password']
         return rep
+
+    def create(self, validated_data):
+        user = User.objects.create(**validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        if validated_data['password']:
+            instance.set_password(validated_data['password'])
+            instance.save()
+        return instance
+
 
 class AgentModelSerializer(serializers.ModelSerializer):
     class Meta:
--- api/views.py
+++ api/views.py
@@ -1,9 +1,4 @@
-from django.shortcuts import get_object_or_404
-from django.contrib.auth import hashers
-from django.contrib.auth.password_validation import validate_password
-
-from rest_framework import status, viewsets
-from rest_framework.response import Response
+from rest_framework import viewsets
 
 from api.models import User, Event, Agent
 from api.serializers import (
@@ -14,46 +9,11 @@ class UserAPIViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserModelSerializer
 
-    def create_password(self, password):
-        validate_password(password)
-        return hashers.make_password(password)
-
-    def create(self, request):
-        user = request.data.dict()
-        user['last_login'] = None
-        user['password'] = self.create_password(user.get('password'))
-
-        serializer = UserModelSerializer(data=user)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-    # https://github.com/encode/django-rest-framework/.../mixins.py#L59
-    def update(self, request, *args, **kwargs):
-        instance = self.get_object()
-        user = request.data
-
-        password = user.get('password')
-        if password:
-            user['password'] = self.create_password(password)
-
-        serializer = UserModelSerializer(
-            instance, data=user, partial=True
-        )
-
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        if getattr(instance, '_prefetched_objects_cache', None):
-            # If 'prefetch_related' has been applied to a queryset, we need to
-            # forcibly invalidate the prefetch cache on the instance.
-            instance._prefetched_objects_cache = {}
-
-        return Response(serializer.data)
-
 
 class EventAPIViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventModelSerializer
+
 
 class AgentAPIViewSet(viewsets.ModelViewSet):
     queryset = Agent.objects.all()
