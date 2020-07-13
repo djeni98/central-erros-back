6d8131cb7acfc4d4fa4c079a8aedd7f693d5f0da
Djenifer R. P <djeniferrenata@yahoo.com.br>
2020-07-13 10:52:04 -0300

    Arruma importação de secret key

--- centralErros/settings.py
+++ centralErros/settings.py
@@ -12,7 +12,11 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 from datetime import timedelta
-from centralErros.secret import SECRET_KEY
+
+try:
+    from centralErros.secret import SECRET_KEY
+except:
+    SECRET_KEY = ''
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
